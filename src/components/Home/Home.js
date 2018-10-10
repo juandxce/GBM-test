4bd17a47dd5e05b188a5684e47610e95b4288fba
juandxce <juandxce@Juandxces-MacBook-Pro.local>
2018-10-10 00:33:32 -0500

    E1-S1-resume of how would I implement levels 2 and bonus

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,11 +7,51 @@ import { fetchChartsData } from "../../store";
 class Home extends React.Component {
 
     render() {
+        let text=`
+        
+        This is the challenge made up to level 2. I'm a little sleepy, so I'm gonna wrap things up:
+        Maybe I was too ambitious with the deliverable and failed to meet the expectations. 
+        The project is a React/Redux app rendered 
+        in the server with express as the "backend". After the loading page, there is a carousel of images 
+        with the observations that I made from your sebsite ( <a>https://www.gbm.com.mx</a> )
+        Is this my best work? absolutely not, If I didn't had so little free time (about 1 - 2  hours a day) The result 
+        would have been awesome!.
+        Level 2- “Let’s Rock”:
+        I would go with Oauth 2, something like 'angular-oauth2-oidc'
+        
+        Then implement it:
+
+        import { OAuthService, OAuthEvent } from 'angular-oauth2-oidc';
+        this.oauthService.userinfoEndpoint = this.baseURL+'rest/user?version='+this.config.version;
+        this.oauthService.clientId = 'clientidabunchoflettersandnumerswasd';
+        this.oauthService.dummyClientSecret = 'clientsecretabunchoflettersandnumerswasd';
+        this.oauthService.events.subscribe(({type}:OAuthEvent)=>{
+            switch (type) {
+            case 'token_expires':
+                this.oauthService.refreshToken();
+            break;
+            }
+        });
+        this.oauthService.fetchTokenUsingPasswordFlow(user,password)
+        .then((resp:any)=>{
+            this.oauthService.loadUserProfile()
+        }
+        .
+        . 
+        .
+
+        For the "bonus" I would go with our backend developer and ask him for the functionality, then implement it on the browser as client-side
+        validation can be bypassed pretty easily
+        I hope that the loading page helps you to improve your site, best regards.`;
         return (
             <div id="homepage" className="page-wrapper page">
                 <div id="chart-ipc" className="container clearfix"></div>
                 <br />
                 <Link className="page__link" to="/loading-page">Loading page</Link>
+                <h1>Hello</h1>
+                <p className="text-wrap">
+                {text}
+                </p>
             </div>
         );
     }
